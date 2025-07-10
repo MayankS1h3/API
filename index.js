03692767ea0972898e03eed5b0f23753398a0aad
Mayank Sharma <102649566+MayankS1h3@users.noreply.github.com>
2025-07-10 23:31:59 +0530

    env files

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("server is still running")
+import express from 'express';
+import {PORT} from './config/env.js';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+});
+
+export default app;
